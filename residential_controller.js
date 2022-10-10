2317005621696232c85ecbfa21c2876f12dec781
JulienCharbonneau <juliencharbonneauqc@gmail.com>
2022-10-10 16:52:55 -0400

    SEQUENCE createCallButtons

--- residential_controller.js
+++ residential_controller.js
@@ -1,33 +1,70 @@
+let elevatorID = 1;
+let floorRequestButtonID = 1;
+let callButtonID = 1;
+
 class Column {
-    constructor(_id, _amountOfFloors, _amountOfElevators) {
+  constructor(_id, _amountOfFloors, _amountOfElevators) {
+    this.id = _id;
+    this.status;
+    this.elevatorList = [];
+    this.callButtonList = [];
+    this.createCallButtons(_amountOfFloors);
+  }
+  createCallButtons = function (_amountOfFloors) {
+    let buttonFloor = 1;
+    console.log(_amountOfFloors);
 
-    };
+    for (let i = 0; i <= _amountOfFloors; i++) {
+      //If it's not the last floor
+      if (buttonFloor < _amountOfFloors) {
+        let callButton = new CallButton(callButtonID, "OFF", buttonFloor, "UP");
+        this.callButtonList.push(callButton);
+        callButtonID++;
+      }
+      if (buttonFloor > 1) {
+        //If it's not the first floor
 
+        let callButton = new CallButton(
+          callButtonID,
+          "OFF",
+          buttonFloor,
+          "Down"
+        );
+        this.callButtonList.push(callButton);
+        callButtonID++;
+      }
+
+      buttonFloor++;
+    }
+  };
 }
 
 class Elevator {
-    constructor(_id, _amountOfFloors) {
-
-    }
-
+  constructor(_id, _amountOfFloors) {}
 }
 
 class CallButton {
-    constructor(_id, _floor, _direction) {
-
-    }
+  constructor(_id, _status, _floor, _direction) {
+    this.ID = _id;
+    this.status = _status;
+    this.floor = _floor;
+    this.direction = _direction;
+  }
 }
-
 class FloorRequestButton {
-    constructor(_id, _floor) {
-
-    }
+  constructor(_id, _status, _floor) {
+    this.ID = _id;
+    this.status = _status;
+    this.floor = _floor;
+  }
 }
 
 class Door {
-    constructor(_id) {
-
-    }
+  constructor(_id, _status) {
+    this.ID = _id;
+    this.status = _status;
+  }
 }
-
-module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door }
+let testColumn = new Column(1, 12, 1);
+console.log("testColumn: ", testColumn);
+module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door };
